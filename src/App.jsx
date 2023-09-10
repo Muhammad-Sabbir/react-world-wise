66d75fa085f75e1ee99daa930b57d46a8c60d67f
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-09-10 11:32:12 +0600

    Chapter 17: React Router Building Single-Page Applications (SPA) >> Tutorial 10: Nested Routes and Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,17 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />{" "}
+          {/* Here we can also use index instead of path="/" index means default*/}
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>LIST</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
