dd49ef2bc4e74b979d63743234e132a5acf0a347
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-09-10 14:00:10 +0600

    Chapter 17: React Router Building Single-Page Applications (SPA) >> Tutorial 11: Implementing the Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import PageNav from "./components/PageNav";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch {
+        alert("There was an error fetching data");
+      } finally {
+        console.log("false triggered");
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <div>
       <BrowserRouter>
@@ -17,8 +41,14 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>LIST</p>} />
-            <Route path="cities" element={<p>List of cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
