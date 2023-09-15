bd6aa5a72ad4ebe844a084a175d08b61611f3b2c
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-09-15 11:33:05 +0600

    Chapter 18: Advanced State Management The Context API >> Tutorial 14. Setting Map Position With Geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,12 +10,19 @@ import {
 } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [searchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
   useEffect(
@@ -24,8 +31,22 @@ function Map() {
     },
     [mapLat, mapLng]
   );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
