99ae0b39a65d95413433e2b9a65969d84b5710b2
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-10-07 20:58:10 +0600

    Chapter 18: Advanced State Management The Context API >> Tutorial 21. Adding Fake Authentication Protecting a Route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
 
@@ -6,11 +8,23 @@ export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-
+  const { isAuthenticated, login } = useAuth();
+  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app");
+    },
+    [isAuthenticated]
+  );
+  function handleSubmit(e) {
+    e.preventDefault();
+    login(email, password);
+  }
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {/* <form className={styles.form}> */}
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
